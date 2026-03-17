7b73aa023aa452a630e0666d0abee64c6a92cece
christianbugingo <bugingo818@gmail.com>
2026-03-17 19:48:24 +0300

    fix ALLOWED_HOSTS

--- umuganda/settings.py
+++ umuganda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3pr&9r5f3^32tc-blv5r^c0v*@7(snxkff@(-$(*y#iam#qy)a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['umuganda-jjo3.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
